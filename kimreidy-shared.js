147c71856e612e1fd6b29424229210b3aefbc966
chiringuito <treidy@hotmail.com>
2026-03-01 21:08:41 -0500

    Add files via upload

--- kimreidy-shared.js
+++ kimreidy-shared.js
@@ -146,4 +146,15 @@
     }
   });
 
+  // Netlify Identity: redirect to /admin/ after login
+  if (window.netlifyIdentity) {
+    window.netlifyIdentity.on('init', function(user) {
+      if (!user) {
+        window.netlifyIdentity.on('login', function() {
+          document.location.href = '/admin/';
+        });
+      }
+    });
+  }
+
 })();
